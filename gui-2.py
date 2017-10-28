f9c3079c6337231ce95875e47d16f50ce3a69ecf
yfliu-csmu <yfliu.csmu@gmail.com>
2017-10-28 22:47:04 +0800

    3.6
    Minor modfication

--- gui-2.py
+++ gui-2.py
@@ -25,23 +25,25 @@ def complementary_seq(seq):
 def clickOK():
   
   seq = list()
-  ss = str()
 
   for base in entry1.get():
     seq.append(base)
   r_seq = seq[::-1]
   c_seq = complementary_seq(r_seq)
-  ss = "5'-" + ''.join(c_seq) + "-3'"
-  v.set(ss)
+  v.set("5'-" + "".join(c_seq) + "-3'")
+
+#
+# === Main ===
+#
 
 wnd.title('我第二個 GUI 程式')
 wnd.geometry('300x80+500+300')
 
 label1  = Label(wnd, text='請輸入 DNA 序列 ', bg='skyblue')
-label2  = Label(wnd, text='顯示互補的 DNA 序列 ', bg='pink')
+label2  = Label(wnd, text='顯示互補的 DNA 序列 ', bg='yellow')
 entry1  = Entry(wnd)
 entry2  = Entry(wnd, textvariable=v)
-button1 = Button(wnd, text='執行', command=clickOK)
+button1 = Button(wnd, text='執行', bg='pink', width=20, command=clickOK)
 
 label1.grid(row=0, column=0)
 label2.grid(row=1, column=0)
