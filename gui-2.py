d91bca1758ea24d570c529319a3b6a590bb87b86
yfliu-csmu <yfliu.csmu@gmail.com>
2017-10-28 22:18:23 +0800

    3.6
    Minior modification

--- gui-2.py
+++ gui-2.py
@@ -47,7 +47,7 @@ label1.grid(row=0, column=0)
 label2.grid(row=1, column=0)
 entry1.grid(row=0, column=1)
 entry2.grid(row=1, column=1)
-button1.grid(row=2, column=1)
+button1.grid(row=2, columnspan=2)
 
 wnd.mainloop()
 
