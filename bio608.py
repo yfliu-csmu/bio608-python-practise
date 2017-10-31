ad270a3aa060942fa211350adc07ab32c1502741
yfliu-csmu <yfliu.csmu@gmail.com>
2017-10-31 10:27:34 +0800

    3.7
    Add help description

--- bio608.py
+++ bio608.py
@@ -7,7 +7,7 @@ Created on Mon Oct 23 22:04:59 2017
 import re, csv
 
 def read_seq(fn):
-
+    """讀取 DNA 序列"""
     with open(fn, 'rt') as f:
         seq = list()      
         for line in f.readlines():
@@ -19,7 +19,7 @@ def read_seq(fn):
     return seq
 
 def complementary_seq(seq):
-    
+    """互補 DNA 序列"""
     c_seq = list()
 
     c_table = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
@@ -29,12 +29,14 @@ def complementary_seq(seq):
 
     return c_seq
     
-def reverse_seq(c_seq):
-
-    return c_seq[::-1]
+def reverse_seq(c_seq): 
+    """反轉 DNA 序列"""
+    c_seq.reverse()
+    
+    return c_seq
 
 def read_t_table(fn):
-    
+    """建立 Translation Table"""
     with open('T_table.csv', 'r') as f:
         t_table = dict()
         for words in csv.reader(f, delimiter = '\t'):
@@ -43,7 +45,7 @@ def read_t_table(fn):
     return t_table
 
 def translation(seq, offset, t_table):
-    
+    """進行轉譯 DNA 序列"""
     codon = str()
     protein = list()
     
@@ -53,6 +55,6 @@ def translation(seq, offset, t_table):
     return protein
 
 def orf(protein):
-   
+    """判斷 Protein 框架"""
     gene = str()
     print (gene.join(protein))
