5906d5e238ea49d6e46d4f8f486a03ac0db3efeb
yfliu-csmu <yfliu.csmu@gmail.com>
2017-11-24 12:54:50 +0800

    3.91

--- parse.py
+++ parse.py
@@ -23,22 +23,16 @@ with open(fn, 'rt') as f:
         else:
             ans = words[46]
         inf.append([str(id), score])
-        
+
 wb = openpyxl.Workbook()
 ws = wb.active
 
-#x=0
+# x=0
 for row in inf:
     ws.append(row)
-  
 
 
 wb.save('test.xlsx')
-=======
-Created on Fri Nov 17 20:10:22 2017
-
-@author: Home
-cats"""
 
 import pandas as pd
 
@@ -46,27 +40,29 @@ fn = '0601-A.TXT'
 id_list = list()
 score_list = list()
 
+
 def get_stats(group):
     return {'count': group.count()}
 
+
 with open(fn, 'rt') as f:
-  for line in f.readlines():
-    words = line.split(sep=' ')
-    id = str(words[0][0:7])
-    score = float(words[41])
-    id_list.append(id)
-    score_list.append(score)
-    
-df = pd.DataFrame(score_list, index = id_list, columns = ["期中考"])
-s = pd.Series(score_list, index = id_list)
+    for line in f.readlines():
+        words = line.split(sep=' ')
+        id = str(words[0][0:7])
+        score = float(words[41])
+        id_list.append(id)
+        score_list.append(score)
+
+df = pd.DataFrame(score_list, index=id_list, columns=["期中考"])
+s = pd.Series(score_list, index=id_list)
 
 
 bins = [40, 50, 60, 70, 80, 90, 100]
 
 cats = pd.cut(df['期中考'], bins, right=False)
 grouped = df['期中考'].groupby(cats)
-bin_counts = grouped.apply(get_stats)
-#bin_counts = grouped.apply(get_stats).unstack()
+#bin_counts = grouped.apply(get_stats)
+bin_counts = grouped.apply(get_stats).unstack()
 print(bin_counts)
 
 bin_counts.index = ['40~50', '50~60', '60~70',
@@ -75,11 +71,10 @@ bin_counts.index.name = 'Score range'
 bin_counts.plot(kind='bar', alpha=0.5, rot=0)
 
 #print (sd[sd < 70 and sd > 60])
-print (df.期中考[df.期中考 < 60])
+print(df.期中考[df.期中考 < 60])
 #print (df.期中考[(df.期中考 > 60 and df.期中考 < 70)])
-print (df.describe())
+print(df.describe())
 df.plot()
 
->>>>>>> 206df32e0372a17340e9f1961c8e173b123d5a5e
-
-
+#df_aa = pd.read_html('http://www.soc-bdr.org/rds/authors/unit_tables_conversions_and_genetic_dictionaries/genetic_code_tables/')
+# print(df_aa)
